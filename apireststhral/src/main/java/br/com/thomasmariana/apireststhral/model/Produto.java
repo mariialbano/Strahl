66893d44c116115db6948c716b17e530b096ad12
thomasmendonca <rdcharacter10@gmail.com>
2024-03-26 14:27:30 -0300

    Criando validação

--- apireststhral/src/main/java/br/com/thomasmariana/apireststhral/model/Produto.java
+++ apireststhral/src/main/java/br/com/thomasmariana/apireststhral/model/Produto.java
@@ -7,6 +7,9 @@ import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,10 +17,22 @@ import lombok.Data;
 public class Produto {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message ="{produto.nome.notblank}")
+    @Size(min= 3, max = 255, message = "{produto.nome.size}")
     private String nome;
+
+    @Positive(message = "{produto.preco.positive}")
     private BigDecimal preco;
+
+    @Positive(message = "{produto.tela.positive}")
     private double tela;
+
+    @Positive(message = "{produto.armazenamento.positive}")
     private int armazenamento;
+
     private String cor;
+
+    @Positive(message = "{produto.camera.positive}")
     private double camera;
 }
--- apireststhral/src/main/java/br/com/thomasmariana/apireststhral/model/Usuario.java
+++ apireststhral/src/main/java/br/com/thomasmariana/apireststhral/model/Usuario.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
@@ -11,9 +13,17 @@ import lombok.Data;
 public class Usuario {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message="{usuario.nome.notblank}")
     private String nome;
+
+    @NotBlank(message="{usuario.email.notblank}")
     private String email;
-    private int tel;
+
+    @NotBlank(message ="{usuario.tel.notblank}")
+    private String tel;
+
+    @NotBlank(message ="{usuario.estado.notblank}")
     private String estado;
 }
 
